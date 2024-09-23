ae899066c28677ddd5cfb29d2b3d2a7519a299b5
marko <marko@marko-Inspiron-N5110>
2024-09-23 14:23:22 +0200

    RunControllerTest passed.

--- runnerz/src/main/java/dev/maximus/runnerz/Application.java
+++ runnerz/src/main/java/dev/maximus/runnerz/Application.java
@@ -24,7 +24,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
+	 
 	@Bean
 	UserHttpClient userHttpClient() {
 		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
--- runnerz/src/main/resources/schema.sql
+++ runnerz/src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS run;
+
 CREATE TABLE IF NOT EXISTS run (
    id INT NOT NULL,
    title varchar(250) NOT NULL,
